cde9f57f0ea7dda4df8223fdc5b137b392667715
lopezton <tony.lopez1013@gmail.com>
2017-08-07 10:03:09 -0500

    Docker Updates

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -2,11 +2,11 @@
 
 #export DOCKER_HOST_IP=$(echo ${DOCKER_HOST:-tcp://192.168.99.100:2376} | cut -d/ -f3 | cut -d: -f1)
 export DOCKER_HOST_IP=localhost
-export RADIX_SERVER_HOST=${DOCKER_HOST_IP}
+export RADIX_SERVER_HOST=${SERVER_HOST_IP:DOCKER_HOST_IP}
 export RADIX_SERVER_PORT=9001
 #export MONGODB_HOST=${DOCKER_HOST_IP}
 export MONGODB_PORT=27017
-export RADIX_DASHBOARD_HOST=${DOCKER_HOST_IP}
+export RADIX_DASHBOARD_HOST=${SERVER_HOST_IP:DOCKER_HOST_IP}
 export RADIX_DASHBOARD_PORT=8080
 
 docker-compose -f docker-compose-radix-mongo.yml up --build -d
